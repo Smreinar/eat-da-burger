339e1d7d1f76b63c390269ff1066efdac7b45069
steven Reinart <stevenreinart123@gmail.com>
2020-06-08 08:52:46 -0700

    database schema/seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Mig Bac');
+INSERT INTO burgers (burger_name) VALUES ('Sot n Hicy');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beacon Cheese Burger', true);
